8f66408deb75ea6d453c86a3834dcd329b7d7700
aondov <ondov.adrian@gmail.com>
2024-05-01 20:00:37 +0200

    Minor fix in data update script

--- output/scripts/get_data.sh
+++ output/scripts/get_data.sh
@@ -10,6 +10,6 @@ rserver=192.168.94.54
 ruser=aondov
 container_name=postgres_container
 
-sshpass -p "$2" ssh -p 22 -t $ruser@$rserver "docker exec -it ${container_name} psql -d ${database} -U ${user} -c 'COPY (SELECT * FROM ${table}) TO STDOUT ${format} HEADER' > ${table}.${format}"
+sshpass -p "$2" ssh -p 22 -q -t $ruser@$rserver "docker exec -it ${container_name} psql -d ${database} -U ${user} -c 'COPY (SELECT * FROM ${table}) TO STDOUT ${format} HEADER' > ${table}.${format}"
 
-sshpass -p "$2" scp -P 22 $ruser@$rserver:/home/$ruser/$table.$format $vulnelora_folder/$table.$format
+sshpass -p "$2" scp -q -P 22 $ruser@$rserver:/home/$ruser/$table.$format $vulnelora_folder/$table.$format
